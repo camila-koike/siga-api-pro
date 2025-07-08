94b90964ab27216756eec8c8cbbf62d2fa0b331a
camila-koike <camilakoike@gmail.com>
2025-07-08 14:49:22 -0400

    findall disciplinas

--- src/disciplinas/disciplinas.service.ts
+++ src/disciplinas/disciplinas.service.ts
@@ -8,10 +8,41 @@ export class DisciplinasService {
   async create(data: any) {
     return await prisma.disciplina.create({ data });
   }
-
   async findAll() {
-    return await prisma.disciplina.findMany();
+    const disciplinas = await prisma.disciplina.findMany({
+      include: {
+        professor: {
+          include: {
+            usuario: true,
+          },
+        },
+      },
+    });
+
+    // Mapeia para devolver só o que interessa
+    return disciplinas.map((disciplina) => ({
+      id: disciplina.id,
+      periodo: disciplina.periodo,
+      turno: disciplina.turno,
+      nome: disciplina.nome,
+      qnt_total_aulas: disciplina.qnt_total_aulas,
+      carga_horaria: disciplina.carga_horaria,
+      curso: disciplina.curso,
+      professor: disciplina.professor
+        ? {
+            id: disciplina.professor.usuario.id,
+            nome: disciplina.professor.usuario.nome,
+            email: disciplina.professor.usuario.email,
+          }
+        : null,
+    }));
   }
+
+ 
+  /*
+   async findAll() {
+    return await prisma.disciplina.findMany();
+  }*/
 
   async findOne(id: number) {
     return await prisma.disciplina.findUnique({ where: { id } });
